b95b45b0fd3404a26f7bf059827f10d535d8b7aa
vaibhav111tandon <vaibhav.tandongcet@gmail.com>
2019-06-08 00:14:53 +0530

    Done some minor changes

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var cursor = document.querySelector('.cursor');
 cursor.addEventListener('mousemove', (e) => {
-   cursor.setAttribute('style', 'top:'+(e.pageY - cursor.clientHeight/2)+'px;left:'+(e.pageX - cursor.clientWidth/2) +'px;'); 
+   cursor.setAttribute('style', 'top:'+(e.clientY + 10 - cursor.clientHeight/2)+'px;left:'+(e.clientX - cursor.clientWidth/2) +'px;'); 
 
 });
 
 var body = document.getElementsByTagName('body')[0];
 body.addEventListener('mouseover', (e) => {
-    cursor.setAttribute('style', 'top:'+(e.pageY - cursor.clientHeight/2)+'px;left:'+(e.pageX - cursor.clientWidth/2) +'px;'); 
+    cursor.setAttribute('style', 'top:'+(e.clientY + 10 - cursor.clientHeight/2)+'px;left:'+(e.clientX - cursor.clientWidth/2) +'px;'); 
 });
 
 body.addEventListener('click', (e) => {
-    cursor.setAttribute('style', 'top:'+(e.pageY - cursor.clientHeight/2)+'px;left:'+(e.pageX - cursor.clientWidth/2) +'px;');
+    cursor.setAttribute('style', 'top:'+(e.clientY + 10 - cursor.clientHeight/2)+'px;left:'+(e.clientX - cursor.clientWidth/2) +'px;');
 });
  
